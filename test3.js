0cf586ce0eae54ac0a85a667649d831597b1336c
GXXXXGX-IFG38A5\Administrator <cbb34678@gmail.com>
2018-03-27 11:29:50 +0800

    TEST3完成

--- test3.js
+++ test3.js
@@ -1,28 +1,29 @@
 // 打开浏览器的时间到vedio标签出现的时间
 const puppeteer = require('puppeteer');
-var colors = require( "colors");
-var urls = require('./config.js')
+const colors = require( "colors");
+const config = require('./config.js');
 (async () => {
-        const browser = await puppeteer.launch({
-           headless: false,
-           executablePath:"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-           slowMo:0
-        });
-
-        const page = await browser.newPage();
         var res = [];
-        await urls.forEach((ele)=>{
+        // 这里不要用foreach  因为会改变async/await 的结构
+        for(var ele in config.urls) {
+           var browser = await puppeteer.launch({
+                headless: false,
+                executablePath:"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+                slowMo:0
+            }).catch(error => console.log(error.message));
+            var page = await browser.newPage().catch(error => console.log(error.message));
             var date  = new Date();
-            page.goto(ele);
-            await page.waitForSelector('video').then(()=>{
-                var date2  = new Date();
-                var interval = date2-date;
+            page.goto(config.urls[ele]);
+            await page.waitForSelector('video').then(() => {
+                var date2 = new Date();
+                var interval = date2 - date;
                 res.push(interval);
                 console.log(colors.red(interval))
-            })  
-        })   
+            }).catch(error => console.log(error.message));
+            browser.close();
+        }
         //等待vedio标签出现 出现了以后打印出时间然后关闭浏览器
         console.log(res);
-        browser.close();
+
     }
 )();
